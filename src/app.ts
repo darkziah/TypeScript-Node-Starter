2ea3b1f073847f82e2791b036f82711049208b7b
darkziah <darkzephanziah@gmail.com>
2021-10-11 23:49:33 +0800

    yehey link
    URL shortener

--- src/app.ts
+++ src/app.ts
@@ -11,36 +11,33 @@ import passport from "passport";
 import bluebird from "bluebird";
 import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
 
-// Controllers (route handlers)
-import * as homeController from "./controllers/home";
-import * as userController from "./controllers/user";
-import * as apiController from "./controllers/api";
-import * as contactController from "./controllers/contact";
-
-// API keys and Passport configuration
-import * as passportConfig from "./config/passport";
-
 // Create Express server
 const app = express();
 
 // Connect to MongoDB
-const mongoUrl = MONGODB_URI;
+const mongoUrl = "mongodb://node2754.myfcloud.com:27017/yehey";
 mongoose.Promise = bluebird;
 
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
-    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+
+mongoose.connect(mongoUrl).then(
+    () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ 
+        const db = mongoose.connection; 
+    db.on("error", console.error.bind(console, "connection error: "));
+    db.once("open", function () {
+        console.log("Connected successfully");
+    });
+},
 ).catch(err => {
     console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
     // process.exit();
 });
-
 // Express configuration
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "pug");
 app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(session({
     resave: true,
     saveUninitialized: true,
@@ -52,18 +49,39 @@ app.use(session({
         }
     })
 }));
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(passport.initialize());
+//app.use(passport.session());
 app.use(flash());
-app.use(lusca.xframe("SAMEORIGIN"));
-app.use(lusca.xssProtection(true));
-app.use((req, res, next) => {
+//app.use(lusca.xframe("SAMEORIGIN"));
+//app.use(lusca.xssProtection(true));
+/**
+ * app.use((req, res, next) => {
     res.locals.user = req.user;
     next();
 });
-app.use((req, res, next) => {
+ */
+
+const URLSchema = new mongoose.Schema({
+  originalUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrlId: {
+    type: String,
+    required: true,
+  },
+  timeStamp: {
+    type: Number,
+    default: 0,
+  },
+});
+const URLs = mongoose.model("shortUrl", URLSchema);
+
+app.use(async (req, res, next) => {
     // After successful login, redirect back to the intended page
-    if (!req.user &&
+    /**
+     * 
+     * if (!req.user &&
     req.path !== "/login" &&
     req.path !== "/signup" &&
     !req.path.match(/^\/auth/) &&
@@ -74,15 +92,64 @@ app.use((req, res, next) => {
         req.session.returnTo = req.path;
     }
     next();
+     
+    const newUr = new URLs({
+        originalUrl: "asd",
+        shortUrlId: "asd",
+        timeStamp: 0
+    });
+
+    await URLs.findOneAndUpdate({
+        originalUrl: "asd3"
+    },
+    {shortUrlId: "asd2"},
+    {
+        new: true,
+        upsert: true
+    }
+    );
+    const url = await URLs.find({});
+
+   */
+    
+    if (req.path === "/edit-url") {
+        res.json("req.body");
+    } else {
+
+        const urlId = req.path.replace("/", "");
+
+        const url: {
+            originalUrl: string;
+            shortUrlId: string;
+            timestamp: number;
+        }[] = await URLs.find({
+            shortUrlId: urlId
+        });
+
+        if (url[0].originalUrl) {
+            if(url[0].originalUrl.includes("http")) {
+                res.redirect(url[0].originalUrl);
+                console.log("includes");
+            } else {
+                res.redirect(`http://${url[0].originalUrl}`);
+                console.log("not_incluse");
+            }
+            
+        } else {
+            res.json(url[0]);
+        }
+        
+    }
+
 });
 
 app.use(
     express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
 );
 
+
 /**
- * Primary app routes.
- */
+ * 
 app.get("/", homeController.index);
 app.get("/login", userController.getLogin);
 app.post("/login", userController.postLogin);
@@ -101,18 +168,18 @@ app.post("/account/password", passportConfig.isAuthenticated, userController.pos
 app.post("/account/delete", passportConfig.isAuthenticated, userController.postDeleteAccount);
 app.get("/account/unlink/:provider", passportConfig.isAuthenticated, userController.getOauthUnlink);
 
-/**
- * API examples routes.
- */
 app.get("/api", apiController.getApi);
 app.get("/api/facebook", passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getFacebook);
 
-/**
- * OAuth authentication routes. (Sign in)
- */
+
 app.get("/auth/facebook", passport.authenticate("facebook", { scope: ["email", "public_profile"] }));
 app.get("/auth/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/login" }), (req, res) => {
+   
     res.redirect(req.session.returnTo || "/");
 });
+ * 
+ */
+
+
 
 export default app;
